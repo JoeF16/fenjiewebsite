2c51484aa79430b384756a917b65d71f1d8dc4e3
JoeF16 <you26mos@gmail.com>
2020-04-11 22:09:58 +0200

    fenjiewebsite

---
+++ ourproductt/js/jquery.js
@@ -0,0 +1,38 @@
+  $(document).ready(function(){
+
+    $(".select-btn").click(function(){
+      $(".p-hidden").fadeToggle(250);
+
+    });
+ 
+    $(".select-btn2").click(function(){
+    $(".p-hidden2").fadeToggle(250);
+
+     });
+
+     $(".select-btn3").click(function(){
+      $(".p-hidden3").fadeToggle(250);
+  
+   });
+
+   $(".select-btn4").click(function(){
+    $(".p-hidden4").fadeToggle(250);
+
+
+   });
+
+   $(".select-btn5").click(function(){
+    $(".p-hidden5").fadeToggle(250);
+
+
+   });
+
+   $(".select-btn6").click(function(){
+    $(".p-hidden6").fadeToggle(250);
+
+
+   });
+
+
+  });  
+  
